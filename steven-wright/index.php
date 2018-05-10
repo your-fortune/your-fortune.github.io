fff3f74b190b67dced79c3a16ec0f5a0f43076cc
Elementalidad Production <jrguitar21@jets.dreamhost.com>
2018-05-10 09:06:07 -0700

    2018 update: new fonts, html5

--- steven-wright/index.php
+++ steven-wright/index.php
@@ -2,22 +2,15 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Your Fortune Cookie</title>
-<link href="https://fonts.googleapis.com/css?family=Pompiere" rel="stylesheet">
+<title>Steven Wright Aphorisms</title>
 <link media="all" href="/style.css" rel="stylesheet"/>
-<style>
-  body {
-    font-family: Pompiere, cursive;
-    font-size: 5vw;
-  }
-</style>
 </head>
 <body>
 <div id="page">
 <div id="content">
-<?php include('./fortune.inc'); ?>
+<?php include('../fortune.inc'); ?>
 <blockquote class="fadeInUp">
-<?php print fortune('-s');?>
+<?php print fortune('-s /home/jrguitar21/www/fortune.elementalidad.com/data/steven-wright');?>
 </blockquote>
 </div></div>
 </body>
