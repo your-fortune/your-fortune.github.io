863b7dec1d78d85a6362e7954f21be372f720193
James Wilson <jrwilson3@gmail.com>
2019-01-28 08:46:31 -0500

    Steven Wright Logo and Favicon

--- steven-wright/index.php
+++ steven-wright/index.php
@@ -3,6 +3,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Steven Wright Aphorisms</title>
+<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+<link rel="manifest" href="site.webmanifest">
+<link rel="mask-icon" href="safari-pinned-tab.svg" color="#5bbad5">
+<meta name="apple-mobile-web-app-title" content="Steven Wright Aphorisms">
+<meta name="application-name" content="Steven Wright Aphorisms">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
 <meta name="keywords" content="Steven Wright, surreal humor, one-liners, deadpan, wit/word play, observational comedy, Musical comedy, Anti-humor" />
 <meta name="description" content="Refresh the page for a new one liner." />
 <link media="all" href="/style.css" rel="stylesheet"/>
