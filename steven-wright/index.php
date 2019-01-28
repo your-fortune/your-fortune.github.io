a24d4327b171e864bc977a82946422eb5f2c402a
James Wilson <jrwilson3@gmail.com>
2019-01-28 07:55:37 -0500

    SEO

--- steven-wright/index.php
+++ steven-wright/index.php
@@ -3,6 +3,8 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Steven Wright Aphorisms</title>
+<meta name="keywords" content="Steven Wright, surreal humor, one-liners, deadpan, wit/word play, observational comedy, Musical comedy, Anti-humor" />
+<meta name="description" content="Refresh the page for a new one liner." />
 <link media="all" href="/style.css" rel="stylesheet"/>
 </head>
 <body>
